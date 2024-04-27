8e9339ada62451b3a587c96544904f23d50bcf97
Amir-BK <Amir.BK@gmail.com>
2024-04-27 19:50:06 +0300

    Add switch on float node

--- Source/SwitchOnVector/Public/K2Node_SwitchOnFloat.h
+++ Source/SwitchOnVector/Public/K2Node_SwitchOnFloat.h
@@ -5,24 +5,18 @@
 #include "CoreMinimal.h"
 #include "K2Node.h"
 #include "K2Node_Switch.h"
-#include "K2Node_SwitchOnVector.generated.h"
+#include "K2Node_SwitchOnFloat.generated.h"
 
 /**
- * This struct is used to pass additional data to the comparison function, the comparison function is static and takes two arguments, the first is the vector to compare
+ * This struct is used to pass additional data to the comparison function, the comparison function is static and takes two arguments, the first is the float to compare
  */
 USTRUCT(BlueprintType)
-struct FVectorAndTolerance
+struct FFloatAndTolerance
 {
 	GENERATED_BODY()
 
 	UPROPERTY()
-	float X = 0.0f;
-
-	UPROPERTY()
-	float Y = 0.0f;
-
-	UPROPERTY()
-	float Z = 0.f;
+	float value = 0.0f;
 
 	UPROPERTY(EditAnywhere, Category = PinOptions)
 	float Tolerance = 0.1f;
@@ -32,7 +26,7 @@ struct FVectorAndTolerance
  * 
  */
 UCLASS()
-class SWITCHONVECTOR_API UK2Node_SwitchOnVector : public UK2Node_Switch
+class SWITCHONVECTOR_API UK2Node_SwitchOnFloat : public UK2Node_Switch
 {
 	GENERATED_BODY()
 
@@ -43,15 +37,15 @@ public:
 
 	//This is the array that will hold the actual vector values for the pins, when the array is changed we will update the pins
 	UPROPERTY(EditAnywhere, Category = PinOptions)
-	TArray<FVector> PinValues;
+	TArray<float> PinValues;
 
 	//The tolerance value for the comparison, passing this to the comparison function is a little tricky, we effectively tack it on to the
-	// case switch vector data in the form of a struct, this is done in the GetExportTextForPin function
+	// case switch float data in the form of a struct, this is done in the GetExportTextForPin function
 	UPROPERTY(EditAnywhere, Category = PinOptions)
 	float Tolerance = 0.1f;
 
 
-	UK2Node_SwitchOnVector();
+	UK2Node_SwitchOnFloat();
 
 	//this method is used to register the node in the blueprint editor
 	virtual void GetMenuActions(FBlueprintActionDatabaseRegistrar& ActionRegistrar) const override;
@@ -59,7 +53,7 @@ public:
 
 
 	UFUNCTION(BlueprintPure, Category = PinOptions, meta = (BlueprintInternalUseOnly = "TRUE"))
-	static bool IsVectorWithToleranceNotNearlyEqual(FVector& A, FVectorAndTolerance& B);
+	static bool IsFloatWithToleranceNotNearlyEqual(float& A, FFloatAndTolerance& B);
 
 	//as we want to display the details view for the node we need to override this function and return true
 	virtual bool ShouldShowNodeProperties() const override { return true; }
@@ -68,8 +62,7 @@ public:
 
 	//K2Node_Switch Interface
 
-	//you need to override this function to create the selection pin and override its data type, in this case we are using a vector
-	//it is not mandatory for the values to be the same as the case pins, you just need a static function that can take both values and return a bool
+
 	virtual void CreateSelectionPin() override;
 
 	//I don't think this one is actually being used
@@ -86,7 +79,6 @@ public:
 	//Generates the export text for the case pins, this is critical as the blueprint compiler uses this text to generate the structs that are used in the comparison function
 	FString GetExportTextForPin(const UEdGraphPin* InPin) const override;
 
-	// For the use case of vectors we don't need the add pin button, the user can just add new values in the array
 	virtual bool SupportsAddPinButton() const { return false; }
 	//End of K2Node_Switch Interface
 
